68f06e430481726268fafb3c8f2bdfba08cec3e4
MrWangJustToDo <2711470541@qq.com>
2023-07-27 19:38:44 +0800

    update

--- backend/src/api/lang.controller.ts
+++ backend/src/api/lang.controller.ts
@@ -1,14 +1,15 @@
-import { Controller, Get, Query, Response } from "@nestjs/common";
+import { Controller, Get, Inject, Query, Response } from "@nestjs/common";
 import { Response as ExpressResponse } from "express";
 
 import { LangService } from "./lang.service";
 
 @Controller("api")
 export class LangController {
-  constructor(private langService: LangService) {}
+  @Inject(LangService)
+  private readonly langService: LangService;
 
   @Get("lang")
-  async getLang(@Query("lang") lang, @Response() response: ExpressResponse) {
+  getLang(@Query("lang") lang: string, @Response() response: ExpressResponse) {
     const stream = this.langService.getLangData(lang);
     if (stream) {
       stream.pipe(response);
